15bf0a6c719bae5080c6df6f1894550f505516a1
Alex Zayets <a.l.e.x.s.c.r.a.t.666@gmail.com>
2016-08-23 08:24:46 +0300

    fix permissions

--- tic_tac/api/v0/games/views.py
+++ tic_tac/api/v0/games/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 from games import models
 from . import serializers
 
@@ -6,3 +6,4 @@ from . import serializers
 class RetrieveGameAPI(generics.RetrieveAPIView):
     queryset = models.Game.objects.all()
     serializer_class = serializers.GameSerializer
+    permission_classes = permissions.AllowAny,
